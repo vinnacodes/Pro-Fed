2a45df5a71a1e6e78141891f4660ea168b78e2e6
vinnarasu <vinnarasu2000@gmail.com>
2025-03-03 18:04:10 +0530

    a cleanup and first custom component

--- pro-fed/src/App.jsx
+++ pro-fed/src/App.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Home from './Homepage/Home'
 function App() {
   
 
   return (
     <>
-      <div>
-       
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Home userName="Aliens"/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ pro-fed/src/Homepage/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+// this is the Home page our users 
+const Home =(p)=>{
+
+
+
+
+    return(
+        <div>
+
+<h2>Welcome {p.userName}</h2>
+
+<p>How are you all?</p>
+
+
+
+
+
+        </div>
+    )
+}
+export default Home;
