feb5cb2f29b59d0e3c1255908ba41e25b9af2474
vinnarasu <vinnarasu2000@gmail.com>
2025-03-05 13:12:37 +0530

    login page created

--- pro-fed/src/Navbar.jsx
+++ pro-fed/src/Navbar.jsx
@@ -5,17 +5,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="Nav">
+      <div className="Nav-logo">
+        <img src="src/assets/sample.jpg" alt="Profile logo" />
+      </div>
       <ul>
-        <li>
-          <img src="src/assets/sample.jpg" alt="Profile logo" />
-        </li>
         <li><Link to="/">Mydashboard</Link></li>
         <li><Link to="/jobs">Jobs</Link></li>
         <li><Link to="/companies">Companies</Link></li>
-        <li><Link to="/events">Events</Link></li>
 
-
-      
+        <li><Link to="/events" >Events</Link></li>
       </ul>
     </div>
   );
